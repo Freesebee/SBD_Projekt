2623d026c80c09c46fb40d8f05e6262451c7a043
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2021-12-06 10:47:14 +0100

    Fix

--- SBD_Projekt/Models/DiscountedProduct.cs
+++ SBD_Projekt/Models/DiscountedProduct.cs
@@ -10,7 +10,6 @@ namespace SBDProjekt.Models
         public int Discount { get; set; }
         public int SaleId { get; set; }
         public Sale Sale { get; set; }
-        public int Discount { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
