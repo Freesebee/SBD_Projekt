e5e2ce93b95866d51e0a09a904393b576aaed2f7
FreeseBee <ajaku@DESKTOP-JTEOCI7>
2021-12-16 13:28:15 +0100

    Authorization in Category CRUD

--- SBD_Projekt/Controllers/CategoriesController.cs
+++ SBD_Projekt/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SBDProjekt.Infrastructure;
@@ -52,6 +53,7 @@ namespace SBD_Projekt.Controllers
         }
 
         // GET: Categories/Create
+        [Authorize(Roles = "Admin"]
         public IActionResult Create()
         {
             return View();
@@ -62,6 +64,7 @@ namespace SBD_Projekt.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin"]
         public async Task<IActionResult> Create([Bind("Id,Name,Description")] Category category)
         {
             if (ModelState.IsValid)
@@ -74,6 +77,7 @@ namespace SBD_Projekt.Controllers
         }
 
         // GET: Categories/Edit/5
+        [Authorize(Roles = "Admin"]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -94,6 +98,7 @@ namespace SBD_Projekt.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin"]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Category category)
         {
             if (id != category.Id)
@@ -125,6 +130,7 @@ namespace SBD_Projekt.Controllers
         }
 
         // GET: Categories/Delete/5
+        [Authorize(Roles = "Admin"]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -145,6 +151,7 @@ namespace SBD_Projekt.Controllers
         // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin"]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var category = await _context.Categories.FindAsync(id);
