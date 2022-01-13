556e2e6e9f6f42fa29f51987468d10670afcc89f
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2022-01-13 18:34:33 +0100

    Cleaning frontend and fixing few bugs

--- SBD_Projekt/Controllers/CategoriesController.cs
+++ SBD_Projekt/Controllers/CategoriesController.cs
@@ -130,42 +130,42 @@ namespace SBD_Projekt.Controllers
         }
 
         // GET: Categories/Delete/5
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
+        //    var category = await _context.Categories
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (category == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(category);
-        }
+        //    return View(category);
+        //}
 
         // POST: Categories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var category = await _context.Categories.FindAsync(id);
-            _context.Categories.Remove(category);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var category = await _context.Categories.FindAsync(id);
+        //    _context.Categories.Remove(category);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool CategoryExists(int id)
         {
             return _context.Categories.Any(e => e.Id == id);
         }
 
-        
+
         public async Task<IActionResult> SelectOptionsAsync()
         {
             EditProductViewModel model = new EditProductViewModel();
--- SBD_Projekt/Controllers/ManufacturersController.cs
+++ SBD_Projekt/Controllers/ManufacturersController.cs
@@ -28,22 +28,22 @@ namespace SBD_Projekt.Controllers
         }
 
         // GET: Manufacturers/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var manufacturer = await _context.Manufacturers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (manufacturer == null)
-            {
-                return NotFound();
-            }
+        //    var manufacturer = await _context.Manufacturers
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (manufacturer == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(manufacturer);
-        }
+        //    return View(manufacturer);
+        //}
 
         // GET: Manufacturers/Create
         public IActionResult Create()
@@ -119,33 +119,33 @@ namespace SBD_Projekt.Controllers
         }
 
         // GET: Manufacturers/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var manufacturer = await _context.Manufacturers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (manufacturer == null)
-            {
-                return NotFound();
-            }
+        //    var manufacturer = await _context.Manufacturers
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (manufacturer == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(manufacturer);
-        }
+        //    return View(manufacturer);
+        //}
 
         // POST: Manufacturers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var manufacturer = await _context.Manufacturers.FindAsync(id);
-            _context.Manufacturers.Remove(manufacturer);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var manufacturer = await _context.Manufacturers.FindAsync(id);
+        //    _context.Manufacturers.Remove(manufacturer);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool ManufacturerExists(int id)
         {
